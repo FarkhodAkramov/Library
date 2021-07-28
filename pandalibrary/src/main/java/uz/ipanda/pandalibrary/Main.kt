46ffe291f65ba9cc0dafd0d654789aba84b3f06d
Admin <farhodakramov01@gmail.com>
2021-07-28 17:51:41 +0500

    add square

--- pandalibrary/src/main/java/uz/ipanda/pandalibrary/Main.kt
+++ pandalibrary/src/main/java/uz/ipanda/pandalibrary/Main.kt
@@ -8,4 +8,10 @@ class SnackbarByFarkhod {
     fun s(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
+
+    fun square(c: Context?, a: Int): Int {
+
+        Toast.makeText(c, "${a * a}", Toast.LENGTH_SHORT).show()
+        return a * a
+    }
 }
